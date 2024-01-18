88e0684fcbca1b0e263e6545abcd53d06719156c
rafael <rafael.zoe26@gmail.com>
2024-01-17 22:08:49 -0300

    fix: issues

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,7 +6,7 @@ import { FileModel } from "./entities/filesModel";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host: "postgres",
   port: 5432,
   username: "postgres",
   synchronize: true,
